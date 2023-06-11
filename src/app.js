a1a37584c7300eed683d18f9a95edc64d7100433
Runneker74 <122167596+Runneker74@users.noreply.github.com>
2023-06-11 23:34:22 +0000

    generador

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pintas = ['♥', '♠', '♣', '♦'];
+  let valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  
+  function generarCarta() {
+    let randomPintas = pintas[Math.floor(Math.random() * pintas.length)];
+    let randomValores = valores[Math.floor(Math.random() * valores.length)];
+    
+    let mostrarCarta = `${randomValores} <br> ${randomPintas}`;
+  
+    document.getElementById("mostrarCarta").innerHTML = mostrarCarta
+    
+  
+  }
+  
   console.log("Hello Rigo from the console!");
 };
