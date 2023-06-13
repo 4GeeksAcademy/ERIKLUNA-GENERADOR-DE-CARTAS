925d5a23d959c370accc13c50bfcb1cee3099a3a
Runneker74 <122167596+Runneker74@users.noreply.github.com>
2023-06-13 01:40:06 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -5,19 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pintas = ['♥','♠','♣','♦',];
+let valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
 window.onload = function() {
-  let pintas = ['♥', '♠', '♣', '♦'];
-  let valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-  
-  function generarCarta() {
-    let randomPintas = pintas[Math.floor(Math.random() * pintas.length)];
-    let randomValores = valores[Math.floor(Math.random() * valores.length)];
-    
-    let mostrarCarta = `${randomValores} <br> ${randomPintas}`;
-  
-    document.getElementById("mostrarCarta").innerHTML = mostrarCarta
-    
-  
-  }
-  
+  generarCarta();
 };
+
+function generarCarta() {
+  let randomPintas = pintas[Math.floor(Math.random() * pintas.length)];
+  let randomValores = valores[Math.floor(Math.random() * valores.length)];
+  
+  let mostrarCarta = `${randomValores} <br> ${randomPintas}`;
+
+  if (randomPintas === '♥' || randomPintas === '♦') {
+    document.getElementById("mostrarCarta").style.color = "red";
+  } else {
+    document.getElementById("mostrarCarta").style.color = "black";
+  }
+  document.getElementById("mostrarCarta").innerHTML = mostrarCarta
+  
+
+}
